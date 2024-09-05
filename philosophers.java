5368d663083202d821be186c0c68c41528d0cc9c
Sarah Haddix <sarahbh@unc.edu>
2024-09-04 23:36:01 -0400

    added short description

--- philosophers.java
+++ philosophers.java
@@ -1,6 +1,14 @@
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
+/*
+ * The algorithm works as follows:
+ * A philosopher will want to eat at a random time, at least every five seconds.
+ * When the philosopher wants to eat, they will attempt to pick up their left fork and then they will attempt to pick up their right fork. The exception to this is the first philosopher, who will attempt to do this in the opposite order. If it is not possible for a philosopher to pick up their forks, they will wait for either one or two seconds. Philosopher 1 will wait for 1 second, philosopher 2 for 2 seconds, philosopher 3 for 1 second, philosopher 4 for 2 seconds, and philosopher 5 for 1 second. This staggered waiting time is so that there is not a scenario where two philosophers next to each other would keep picking up one fork and putting it back down at exactly the same time, causing a loop and deadlock.
+ * When a philosopher is able to eat, they will eat for one second and then put down their forks and wait for 5 seconds. This is done so that the philosophers next to them will be able to fulfill an eating turn in that time, preventing starvation. 
+ * This process repeats indefinitely.
+ */
+
 public class philosophers extends Thread {
     public static Lock[] forks = {new ReentrantLock(), new ReentrantLock(), new ReentrantLock(), new ReentrantLock(), new ReentrantLock()};
 
